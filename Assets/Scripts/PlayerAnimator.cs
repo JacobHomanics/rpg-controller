166f5594f8f6aeb81b8f3b99abda7672d69b2f5c
Jacob Homanics <homanicsjake@gmail.com>
2025-02-27 14:45:05 -0500

    updated animator

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -7,10 +7,21 @@ public class PlayerAnimator : MonoBehaviour
 
     public float damping = 0.05f;
 
+    public string horizontal = "X";
+    public string vertical = "Z";
+
+    public string jump = "Jump";
+    public bool triggerBasedJump;
+
+    public bool isUsingIsMoving;
+    public string isMoving = "IsMoving";
+
     public void Jump()
     {
-        // anim.SetTrigger("Jump");
-        anim.Play("Jump", 1, 0f);
+        if (triggerBasedJump)
+            anim.SetTrigger(jump);
+        else
+            anim.Play(jump, 1, 0f);
     }
 
 
@@ -18,30 +29,33 @@ public class PlayerAnimator : MonoBehaviour
     {
         var localized = controller.NormalizedInputMoveDirection;
 
+        if (isUsingIsMoving)
+            anim.SetBool(isMoving, localized.x != 0 || localized.z != 0);
+
         if (localized.x > 0)
         {
-            anim.SetFloat("X", 1f, damping, Time.deltaTime);
+            anim.SetFloat(horizontal, 1f, damping, Time.deltaTime);
         }
         else if (localized.x < 0)
         {
-            anim.SetFloat("X", -1f, damping, Time.deltaTime);
+            anim.SetFloat(horizontal, -1f, damping, Time.deltaTime);
         }
         else
         {
-            anim.SetFloat("X", 0f, damping, Time.deltaTime);
+            anim.SetFloat(horizontal, 0f, damping, Time.deltaTime);
         }
 
         if (localized.z > 0)
         {
-            anim.SetFloat("Z", 1f, damping, Time.deltaTime);
+            anim.SetFloat(vertical, 1f, damping, Time.deltaTime);
         }
         else if (localized.z < 0)
         {
-            anim.SetFloat("Z", -1f, damping, Time.deltaTime);
+            anim.SetFloat(vertical, -1f, damping, Time.deltaTime);
         }
         else
         {
-            anim.SetFloat("Z", 0f, damping, Time.deltaTime);
+            anim.SetFloat(vertical, 0f, damping, Time.deltaTime);
         }
     }
 }
